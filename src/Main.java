7dfef4bf5b7158116916f941d2b779748a284a1c
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-02-27 18:07:20 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Cycles theCycles = new Cycles();
+
+        theCycles.task1();
+        theCycles.task2();
+        theCycles.task3();
+        theCycles.task4();
+        theCycles.task5();
+        theCycles.task6();
+        theCycles.task7();
+        theCycles.task8();
+        theCycles.task9();
+        theCycles.task10();
     }
 }
